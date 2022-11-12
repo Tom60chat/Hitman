d9a16649aa1e89f4c48edd077cf981573b46d52e
Tom Olivier <Tom602001@live.fr>
2022-11-12 13:46:46 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Tom OLIVIER 2022");
+
+if (args.Length > 1 &&
+    int.TryParse(args[0], out int contractor_id) &&
+    int.TryParse(args[1], out int target_id))
+{
+    try
+    {
+        var contractor = Process.GetProcessById(contractor_id);
+        var target = Process.GetProcessById(target_id);
+
+        contractor.WaitForExit();
+        target.Kill();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+else
+    Console.WriteLine("Hitman.exe [contractor_id] [target_id}");
